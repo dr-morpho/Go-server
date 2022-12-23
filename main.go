6784e7fbfdae144ad4bb0a41175206f54c146dc6
Rodion Minaev <rodionminaev96@gmail.com>
2022-12-23 11:53:52 +0300

    feat: server

--- main.go
+++ main.go
@@ -1,2 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"go-server/utils"
+	"log"
+	"net/http"
+)
+
+func main(){
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", utils.FormHandler)
+	http.HandleFunc("/hello", utils.HelloHandler)
+
+	fmt.Printf("Port server: 8080 \n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+} 
---
+++ utils/utils.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "this methood is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+}
+
+func FormHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
